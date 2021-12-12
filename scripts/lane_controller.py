e7681a9648bececbaf95e31222ee185e80bcf841
Muizz_laptop <abmuizz@gmail.com>
2021-12-12 21:01:50 +0100

    Update controller with keyboard

--- scripts/lane_controller.py
+++ scripts/lane_controller.py
@@ -1,20 +1,55 @@
 #!/usr/bin/env python
  
 import rospy
-import numpy as np
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
+import sys, select, tty, termios
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def getch():
+    # fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(sys.stdin.fileno())
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+        return ch
+
+msg = """
+Lane Following Menu
+---------------------------
+Start/Stop:
+
+space key or s
+
+CTRL-C to quit
+"""
+
 
 class ControlLane():
     def __init__(self):
         self.sub_lane = rospy.Subscriber('/detect/lane', Float64, self.cbFollowLane, queue_size = 1)
         self.sub_max_vel = rospy.Subscriber('/control/max_vel', Float64, self.cbGetMaxVel, queue_size = 1)
         self.pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
-        
+        # print(getKey(),'1')
         self.lastError = 0
         self.prevError = 0
         self.angular_z = 0
         self.MAX_VEL = 0.2
+        self.run = False
+        self.status = 0
+        print(msg)
 
         rospy.on_shutdown(self.fnShutDown)
 
@@ -22,29 +57,49 @@ class ControlLane():
         self.MAX_VEL = max_vel_msg.data
 
     def cbFollowLane(self, desired_center):
+        # print(getch())
+        key = getch()
+        if key == ' ' or key == 's' :
+            self.run = not self.run
 
+        if self.status == 20 :
+                print(msg)
+                self.status = 0
+        # key = getKey()
         center = desired_center.data
+        if self.run == True:
+            error = center - 500
 
-        error = center - 500
+            Kp = 0.0006
+            Kd = 0.0004
 
-        Kp = 0.0006
-        Kd = 0.0005
+            self.angular_z += Kp * (error - self.lastError) + Kd * (error - 2 * self.prevError + self.lastError)
+            self.prevError = self.lastError
+            self.lastError = error
+            twist = Twist()
+            twist.linear.x = 0.1        
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = -max(self.angular_z, -3.0) if self.angular_z < 0 else -min(self.angular_z, 3.0)
+            self.pub_cmd_vel.publish(twist)
+            self.status +=  1
 
-        self.angular_z += Kp * (error - self.lastError) + Kd * (error - 2 * self.prevError + self.lastError)
-        self.prevError = self.lastError
-        self.lastError = error
-        twist = Twist()
-        twist.linear.x = 0.1        
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = -max(self.angular_z, -3.0) if self.angular_z < 0 else -min(self.angular_z, 3.0)
-        self.pub_cmd_vel.publish(twist)
+        elif self.run == False:
+            self.stop()
+            self.status +=  1
+
+        elif key == '\x03':
+            # print(key)
+            rospy.signal_shutdown('ctrl-c is pressed. Quitting now')
+                  
 
     def fnShutDown(self):
         rospy.loginfo("Shutting down. cmd_vel will be 0")
+        self.stop()
 
+    def stop(self):
         twist = Twist()
         twist.linear.x = 0
         twist.linear.y = 0
@@ -52,13 +107,19 @@ class ControlLane():
         twist.angular.x = 0
         twist.angular.y = 0
         twist.angular.z = 0
-        self.pub_cmd_vel.publish(twist) 
+        self.pub_cmd_vel.publish(twist)
 
     def main(self):
         rospy.spin()
 
 if __name__ == '__main__':
-    rospy.init_node('control_lane')
-    rospy.loginfo('lane following controller is running')
-    node = ControlLane()
-    node.main()
+    settings = termios.tcgetattr(sys.stdin)
+    try:
+        rospy.init_node('control_lane')
+        rospy.loginfo('lane following controller is running')
+        node = ControlLane()
+        node.main()
+        
+    except rospy.ROSInterruptException:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        pass
--- scripts/lane_detection.py
+++ scripts/lane_detection.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
+
 import rospy
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import CompressedImage
 import numpy as np
 from std_msgs.msg import Float64
 import cv2
 from cv_bridge import CvBridge
-import time
 
 class lane_following:
     def __init__(self):
@@ -122,23 +122,23 @@ class lane_following:
         
         
         if leftx_base < 100 and rightx_base > 600:
-            cx, cy, _, _ = getPerpCoord(right_fitx[300], 300, right_fitx[301], 301, 270)
+            cx, cy, _, _ = getPerpCoord(right_fitx[300], 300, right_fitx[301], 301, 260)
             img = cv2.circle(img, (cx,cy), radius=10, color=(0, 0, 255), thickness=-1)
             right_lane=np.array([np.transpose(np.vstack([right_fitx, ploty]))])
             final = cv2.polylines(img, np.int_([right_lane]), isClosed=False, color=(255, 255, 255), thickness=24)
-            if (time.time() - self.prev_time) > 20.0 and not self.log_status == 'right':
+            if not self.log_status == 'right':
                 rospy.loginfo('Detect only right lane')
-                self.prev_time = time.time()
+                # self.prev_time = time.time()
                 self.log_status = 'right'
 
         elif leftx_base > 100 and rightx_base < 600:
-            cx, cy, _, _ = getPerpCoord(left_fitx[300], 300, left_fitx[301], 301, -270)
+            cx, cy, _, _ = getPerpCoord(left_fitx[300], 300, left_fitx[301], 301, -260)
             img = cv2.circle(img, (cx,cy), radius=10, color=(0, 0, 255), thickness=-1)
             left_lane=np.array([np.transpose(np.vstack([left_fitx, ploty]))])
             final = cv2.polylines(img, np.int_([left_lane]), isClosed=False, color=(0, 255, 255), thickness=24)
-            if (time.time() - self.prev_time) > 20.0 and not self.log_status == 'left':
+            if not self.log_status == 'left':
                 rospy.loginfo('Detect only left lane')
-                self.prev_time = time.time()
+                # self.prev_time = time.time()
                 self.log_status = 'left'
 
         elif leftx_base > 100 and rightx_base > 600:
@@ -148,17 +148,17 @@ class lane_following:
             right_lane=np.array([np.transpose(np.vstack([right_fitx, ploty]))])
             final = cv2.polylines(img, np.int_([left_lane]), isClosed=False, color=(0, 255, 255), thickness=24)
             final = cv2.polylines(final, np.int_([right_lane]), isClosed=False, color=(255, 255, 255), thickness=24)
-            if (time.time() - self.prev_time) > 20.0 and not self.log_status == 'both':
+            if not self.log_status == 'both':
                 rospy.loginfo('Detect both lanes')
-                self.prev_time = time.time()
+                # self.prev_time = time.time()
                 self.log_status = 'both'
 
         else:
             cx = 500
             final = img
-            if (time.time() - self.prev_time) > 20.0 and not self.log_status == None:
+            if not self.log_status == None:
                 rospy.loginfo('There is no lane')
-                self.prev_time = time.time()
+                # self.prev_time = time.time()
                 self.log_status = None
  
         # write lane center point pixel x to publish
